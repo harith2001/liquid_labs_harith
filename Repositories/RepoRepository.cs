8ad4b37294b26b9712ec98ede90d4d70d65f88b1
harith2001 <danula243@gmail.com>
2026-02-26 22:47:07 +0530

    Data Access layer added

---
+++ Repositories/RepoRepository.cs
@@ -0,0 +1,59 @@
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+
+public class RepoRepository
+{
+    private readonly IDBConnectionFactory _connectionFactory;
+    public RepoRepository(IDBConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    public async Task<Repository?> GetByFullNameAsync (string fullName)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        await connection.OpenAsync();
+        
+        string query =@"
+            SELECT rp.*, o.Id as OwnerId, o.GithubId as OwnerGithubId, o.Login, o.Url, o.Type
+            FROM Repositories rp
+            INNER JOIN Owners o ON rp.OwnerID = o.Id
+            WHERE rp.FullName = @FullName";
+
+        // paramterized query
+        using var command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@FullName", fullName);
+
+        using var reader = await command.ExecuteReaderAsync();
+
+        // check validation
+        if(!reader.HasRows)
+        return null;
+
+        await reader.ReadAsync();
+
+        return new Repository
+        {
+            Id = reader.GetInt32(reader.getOrdinal("Id")),
+            GithubId = reader.GetInt64(reader.GetOrdinal("GithubId")),
+            Name = reader.GetString(reader.GetOrdinal("Name")),
+            FullName = reader.GetString(reader.GetOrdinal("FullName")),
+            Description = reader["Description"] as string,
+            StargazersCount = reader.GetInt32(reader.GetOrdinal("StargazersCount")),
+            ForksCount = reader.GetInt32(reader.GetOrdinal("ForksCount")),
+            OpenIssuesCount = reader.GetInt32(reader.GetOrdinal("OpenIssuesCount")),
+            Language = reader["Language"] as string,
+            CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+            UpdatedAt = reader.GetDateTime(reader.GetOrdinal("UpdatedAt")),
+            Owner = new Owner
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                GithubId = reader.GetInt64(reader.GetOrdinal("OwnerGithubId")),
+                Login = reader.GetString(reader.GetOrdinal("Login")),
+                Url = reader["Url"] as string,
+                Type = reader["Type"] as string
+            }
+        };
+    }
+}
