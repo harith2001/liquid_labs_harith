873d0719b87f031afc496e5baf80552e9d4c646b
harith2001 <danula243@gmail.com>
2026-02-26 22:27:46 +0530

    Defining the Schema

---
+++ Models/Owner.cs
@@ -0,0 +1,10 @@
+public class Owner
+{
+    public int ID {get; set;}
+    public long GithubID {get; set;}
+    public string Login {get; set;}
+    public string Url {get; set;}
+    public string Type {get; set;}
+    public DateTime CreatedAt { get; set; }
+
+}
