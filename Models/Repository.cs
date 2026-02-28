3caeda3e81b0f78c7900e8066d9f61b830fb8b19
harith2001 <danula243@gmail.com>
2026-02-28 08:45:33 +0530

    added the nullable for the coloum

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -1,16 +1,16 @@
 public class Repository
 {
-    public int ID { get; set; }
-    public long GithubID { get; set; }
-    public string Name { get; set; }
-    public string FullName { get; set; }
-    public string Descriprion { get; set; }
+    public int Id { get; set; }
+    public long GithubId { get; set; }
+    public string? Name { get; set; }
+    public string? FullName { get; set; }
+    public string? Description { get; set; }
     public int StargazersCount { get; set; }
     public int ForksCount { get; set; }
     public int OpenIssuesCount { get; set; }
-    public string Language { get; set; }
+    public string? Language { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
-    public Owner Owner { get; set; }
+    public Owner? Owner { get; set; }
 
 }
